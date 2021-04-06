8be7d1328faca14ca3beb515a28bdd997e231e2a
Andre <domasniy@mail.ru>
2021-04-06 15:19:48 +0000

    new version

--- app/run_server.py
+++ app/run_server.py
@@ -10,13 +10,14 @@ import pandas as pd
 import os
 dill._dill._reverse_typemap['ClassType'] = type
 #import cloudpickle
-import flask
+from flask import Flask, jsonify, request
+
 import logging
 from logging.handlers import RotatingFileHandler
 from time import strftime
 
 # initialize our Flask application and the model
-app = flask.Flask(__name__)
+app = Flask(__name__)
 model = None
 
 handler = RotatingFileHandler(filename='app.log', maxBytes=100000, backupCount=10)
@@ -40,46 +41,37 @@ def general():
 
 @app.route("/predict", methods=["POST"])
 def predict():
-	# initialize the data dictionary that will be returned from the
-	# view
-	data = {"success": False}
-	dt = strftime("[%Y-%b-%d %H:%M:%S]")
-	# ensure an image was properly uploaded to our endpoint
-	if flask.request.method == "POST":
+	try:
+		test_json = request.get_json()
+		test = pd.read_json(test_json, orient='records')
 
-		description, company_profile, benefits = "", "", ""
-		request_json = flask.request.get_json()
-		logger.info(request_json)
-		if request_json["description"]:
-			description = request_json['description']
+	except Exception as e:
+		raise e
 
-		if request_json["company_profile"]:
-			company_profile = request_json['company_profile']
 
-		if request_json["benefits"]:
-			benefits = request_json['benefits']
-		logger.info(f'{dt} Data: description={description}, company_profile={company_profile}, benefits={benefits}')
-		try:
-			preds = model.predict_proba(pd.DataFrame({"description": [description],
-												  "company_profile": [company_profile],
-												  "benefits": [benefits]}))
-		except AttributeError as e:
-			logger.warning(f'{dt} Exception: {str(e)}')
-			data['predictions'] = str(e)
-			data['success'] = False
-			return flask.jsonify(data)
+	if test.empty:
+		return (bad_request())
+	else:
 
-		data["predictions"] = preds[:, 1][0]
-		# indicate that the request was a success
-		data["success"] = True
+		print("doing predictions now...")
+		preds = model.predict_proba(test)
+		print(preds)
+		data = {'predictions' : preds.tolist()}
+		print(type(data['predictions']))
+		#prediction_series = list(pd.Series(preds))
 
-	# return the data dictionary as a JSON response
-	return flask.jsonify(data)
+		#final_predictions = pd.DataFrame(prediction_series)
+
+		responses = jsonify(data)
+		responses.status_code = 200
+
+		return responses
+
 
 # if this is the main thread of execution first load the model and
 # then start the server
 if __name__ == "__main__":
 	print(("* Loading the model and Flask starting server..."
 		"please wait until server has fully started"))
-	port = int(os.environ.get('PORT', 8180))
+	port = int(os.environ.get('PORT', 8020))
 	app.run(host='0.0.0.0', debug=True, port=port)
